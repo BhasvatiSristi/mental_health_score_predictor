0be3df8e50918df3ad40106912f202d5c0b13b04
Bhasvati Sristi <bhasvati2106@gmail.com>
2026-07-28 14:46:58 +0530

    seperating pydantic models from main

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,23 @@
 import joblib
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
-from typing import Literal
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
+from backend.models import PredictionResponse, StudentData
 
 model = joblib.load('backend/prediction_model.pkl')
 
+# app setup
 app = FastAPI()
 
-class StudentData(BaseModel):
-    age                     : int = Field(..., ge=10, le=30)
-    gender                  : Literal['Male', 'Female']
-    country                 : str
-    academic_level          : Literal['Undergraduate', 'Graduate', 'High School']
-    most_used_platform      : Literal['Facebook', 'LinkedIn', 'Instagram', 'Snapchat','Twitter','YouTube', 'TikTok', 'LINE', 'KakaoTalk', 'VKontakte', 'WhatsApp','WeChat']
-    purpose_of_use          : Literal['Networking', 'Education', 'Entertainment', 'News']
-    avg_daily_usage_hours   : float = Field(..., ge=0, le=24)
-    daily_unlocks           : int   = Field(..., ge=0)
-    study_hours             : float = Field(..., ge=0, le=24)
-    physical_activity_hours : float = Field(..., ge=0, le=24)
-    sleep_hours_per_night   : float = Field(..., ge=0, le=24)
-    stress_level            : Literal['Medium', 'Low', 'Very High', 'High']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-class PredictionResponse(BaseModel):
-    predicted_mental_health_score : float
-
-
-
+# API endpoints
 @app.get('/')
 def greet():
     return {'message':"Mental Health Score Predictor API"}
